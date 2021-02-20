9ec80926d9d6bd8e58abefd5713e12bb87c4f2be
Michael Phillips <michaeljoelphillips@gmail.com>
2021-02-19 23:19:37 -0600

    Fix indention

--- rdm6300.h
+++ rdm6300.h
@@ -2,33 +2,33 @@
 #include <Stream.h>
 
 class RDM6300: public RFIDReader {
-  public:
-    RDM6300(Stream *stream);
-    unsigned long read();
+	public:
+		RDM6300(Stream *stream);
+		unsigned long read();
 
-  private:
-    /*
-     * RFID Data Frame:
-     *   1 byte head (value: 2)
-     *   2 byte version
-     *   8 byte data
-     *   2 byte checksum
-     *   1 byte tail (value: 3)
-     */
-    static const int BUFFER_SIZE = 14;
+	private:
+		/*
+		 * RFID Data Frame:
+		 *   1 byte head (value: 2)
+		 *   2 byte version
+		 *   8 byte data
+		 *   2 byte checksum
+		 *   1 byte tail (value: 3)
+		 */
+		static const int BUFFER_SIZE = 14;
 
-    /*
-     * The buffer storing data from the RFID serial connection.
-     */
-    int buffer[BUFFER_SIZE];
+		/*
+		 * The buffer storing data from the RFID serial connection.
+		 */
+		int buffer[BUFFER_SIZE];
 
-    /*
-     * The current index of RFID buffer
-     */
-    int buffer_index = 0;
+		/*
+		 * The current index of RFID buffer
+		 */
+		int buffer_index = 0;
 
-    Stream *stream;
+		Stream *stream;
 
-    unsigned long decode_tag();
-    bool verify_checksum();
+		unsigned long decode_tag();
+		bool verify_checksum();
 };
